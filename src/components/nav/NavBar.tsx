871c86c2b7649c9343688a37786ba3d0f77356f3
Timothy <dinosauli2006@gmail.com>
2026-02-07 18:03:02 -0500

    fix update navbar style to store the status in localStorage, update ci, cd workflow dependencies

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -11,13 +11,14 @@ export function BottomBar({ width }: { width: number }) {
 }
 
 export function SideBar({ width }: { width: number }) {
-  const [sideNavOpen, setSideNavOpen] = useState(true);
+  const [sideNavOpen, setSideNavOpen] = useState(localStorage.getItem("sideNavOpen") ? JSON.parse(localStorage.getItem("sideNavOpen") as string) : true);
   return width >= 800 ? (
     <nav className={`sideNav ${sideNavOpen ? "sideNavOpen" : "sideNavClose"}`}>
       <BaseBarComponents width={width} sideNavOpen={sideNavOpen}/>
-      <div className="sideNavHover" onClick={() => 
+      <div className="sideNavHover" onClick={() => {
         setSideNavOpen(!sideNavOpen)
-      }>
+        localStorage.setItem("sideNavOpen", JSON.stringify(!sideNavOpen))
+      }}>
         <RxHamburgerMenu size={"2em"}/>
       </div>
     </nav>
